48a62321797cabd4d7316c9c7ae1802de8f5593e
Aakash Sasikumar <aakash3697@gmail.com>
2016-12-22 19:05:32 +0530

    fixed bugs
    fixed gui freezing bug and others

--- GUI/src/SongPanel.java
+++ GUI/src/SongPanel.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Aakash on 12/15/2016.
@@ -10,6 +11,7 @@ public class SongPanel extends JPanel {
     private JLabel song;
     private JTextField enterSong;
     private JButton search;
+    static String songName;
     SongPanel(){
         song = new JLabel();
         enterSong = new JTextField("", 30);
@@ -20,19 +22,25 @@ public class SongPanel extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 TextAreaAndProgressBar.addText("Scraping YouTube...");
-                String song = enterSong.getText();
+                songName = enterSong.getText();
                // TextAreaAndProgressBar.addText("Scraping YouTube...");
-                ScrapeYouTube.startScrape(song);
+
+                //ScrapeYouTube.startScrape(songName);
                // System.out.println(song);
+                SwingWorker<Void, Void> ob = new ScrapeYouTubeThread();
+                ob.execute();
             }
         });
         enterSong.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 TextAreaAndProgressBar.addText("Scraping YouTube...");
-                String song = enterSong.getText();
 
-                ScrapeYouTube.startScrape(song);
+                songName = enterSong.getText();
+                SwingWorker<Void, Void> ob = new ScrapeYouTubeThread();
+                ob.execute();
+
+                //ScrapeYouTube.startScrape(songName);
             }
         });
         add(song);
